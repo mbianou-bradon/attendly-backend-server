d8d68a2b5c8e45d26496f00564e62d946cd954ce
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-05-23 18:48:01 +0100

    Course Model created

--- models/courseModel.ts
+++ models/courseModel.ts
@@ -3,10 +3,16 @@ import mongoose from "mongoose";
 const courseSchema = new mongoose.Schema({
     courseTitle : {
         type : String,
-        required : [true, "Course title required"]
+        required : [true, "Course title required"],
+        unique : [true, "Course title already exist"]
     },
     courseCode : {
         type : String,
-        required : [true, "Course code required"]
+        required : [true, "Course code required"],
+        unique : [true, "Course code already used by another course, Give a different course code"]
+    },
+    openForAttendance : {
+        type : Boolean,
+        required : [true, "Course status is required"]
     }
 })
